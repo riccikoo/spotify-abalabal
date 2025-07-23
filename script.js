1962e9ab85afdd40e0dfb6bbda8c18dedff79794
riccikoo <reivalaura@gmail.com>
2025-07-23 13:49:47 +0700

    sync

--- script.js
+++ script.js
@@ -7,43 +7,43 @@ let isPlaying = false;
 
 // Lirik: [waktu (detik), "teks"]
 const lyrics = [
-  [24, "Well, when you go"],
-  [29, "Don't ever think I'll make you try to stay"],
-  [36, "And maybe when you get back"],
-  [40, "I'll be off to find another way"],
+  [27, "Well, when you go"],
+  [32, "Don't ever think I'll make you try to stay"],
+  [39, "And maybe when you get back"],
+  [43, "I'll be off to find another way"],
 
-  [46, "And after all this time that you still owe"],
-  [52, "You're still a good-for-nothing I don't know"],
-  [58, "So take your gloves and get out"],
-  [62, "Better get out while you can"],
+  [49, "And after all this time that you still owe"],
+  [55, "You're still a good-for-nothing I don't know"],
+  [61, "So take your gloves and get out"],
+  [65, "Better get out while you can"],
 
-  [67, "When you go"],
-  [70, "Would you even turn to say"],
-  [74, "I don't love you like I did yesterday"],
+  [70, "When you go"],
+  [73, "Would you even turn to say"],
+  [77, "I don't love you like I did yesterday"],
 
-  [85, "Sometimes I cry so hard from pleading"],
-  [90, "So sick and tired of all the needless beating"],
-  [97, "But baby, when they knock you down and out"],
-  [102, "It's where you ought to stay"],
+  [88, "Sometimes I cry so hard from pleading"],
+  [93, "So sick and tired of all the needless beating"],
+  [100, "But baby, when they knock you down and out"],
+  [105, "It's where you ought to stay"],
 
-  [108, "And after all the blood that you still owe"],
-  [113, "Another dollar's just another blow"],
-  [120, "So fix your eyes and get up"],
-  [124, "Better get up while you can"],
+  [111, "And after all the blood that you still owe"],
+  [116, "Another dollar's just another blow"],
+  [123, "So fix your eyes and get up"],
+  [127, "Better get up while you can"],
 
-  [129, "Whoa, whoa"],
+  [132, "Whoa, whoa"],
 
-  [134, "When you go"],
-  [138, "Would you even turn to say"],
-  [143, "I don't love you like I did yesterday"],
+  [137, "When you go"],
+  [141, "Would you even turn to say"],
+  [146, "I don't love you like I did yesterday"],
 
-  [153, "Well come on, come on"],
-  [158, "When you go"],
-  [162, "Would you have the guts to say"],
-  [167, "I don't love you like I loved you yesterday"],
+  [156, "Well come on, come on"],
+  [161, "When you go"],
+  [165, "Would you have the guts to say"],
+  [170, "I don't love you like I loved you yesterday"],
 
-  [175, "I don't love you like I loved you yesterday"],
-  [183, "I don't love you like I loved you yesterday"]
+  [178, "I don't love you like I loved you yesterday"],
+  [186, "I don't love you like I loved you yesterday"]
 ];
 
 // Render lirik ke dalam container
